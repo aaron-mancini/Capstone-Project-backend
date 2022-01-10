fe74b68905d57246daa7c2df82581ea08ce99b75
aaron-mancini <aaronmancini2301@gmail.com>
2022-01-10 10:39:59 -0500

    db, sql helper, expressErrors, and start of user model

---
+++ models/user.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { sqlForPartialUpdate } = require("../helpers/sql");
+const {
+    NotFoundError,
+    BadRequestError,
+    UnauthorizedError,
+} = require("../expressError");
